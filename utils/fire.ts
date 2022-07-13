6b3b13e2b675448c2dd148c48b030e800d170a9e
ryotaro.shibatani <ryotaro.shibatani@iridge.jp>
2022-07-13 22:19:47 +0900

    一覧画面作成

--- utils/fire.ts
+++ utils/fire.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDJwdmWnGEGkis5vp8sd6PHthaUH8_jb5M",
@@ -10,3 +11,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db
